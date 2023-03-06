6606aa56ba6ed17d058cb3f1001807083823580a
MouhamadAlAssaad <mhamad.said.2182000@gmail.com>
2023-03-06 11:23:22 +0200

    fixed the sidebar

--- lms/src/Dashboard/sidebar.js
+++ lms/src/Dashboard/sidebar.js
@@ -2,6 +2,16 @@ import { useState } from "react";
 import {  useLocation , NavLink} from "react-router-dom";
 import "./sidebar.css"
 import logo from "./lms.svg"
+import calender from "./icons/calendar.png"
+import logout from "./icons/logout.png"
+import presentation from "./icons/presentation.png"
+import report from "./icons/report.png"
+import sections from "./icons/sections.png"
+import student from "./icons/student.png"
+import user from "./icons/user.png"
+
+
+
 
 function Sidebar(props) {
   const [activeTab, setActiveTab] = useState(1);
@@ -15,52 +25,61 @@ function Sidebar(props) {
     <div className="sidebar">
     <img className="sidebar_image" src={logo} alt="Logo" />
       <nav>
-        <NavLink
+        
+        <NavLink 
           to="/admins"
-          className={location.pathname === "/admins" ? "active" : ""}
+        className='sidebar-links'
+
           onClick={() => handleTabClick(1)}
-        >
+
+        >    <img className="sidebar_icons" src={user} alt="Logo" />
+
           Admin
         </NavLink>
         <NavLink
           to="/students"
-          className={location.pathname === "/students" ? "active" : ""}
+          className='sidebar-links'
+
           onClick={() => handleTabClick(1)}
-        >
+        ><img className="sidebar_icons" src={student} alt="Logo" />
           Students
         </NavLink>
         <NavLink
           to="/classes"
-          className={location.pathname === "/classes" ? "active" : ""}
+          className='sidebar-links'
+
           onClick={() => handleTabClick(2)}
-        >
+        ><img className="sidebar_icons" src={presentation} alt="Logo" />
           Class
         </NavLink>
         <NavLink
+        className='sidebar-links'
           to="/sections"
-          className={location.pathname === "/sections" ? "active" : ""}
           onClick={() => handleTabClick(3)}
-        >
+        ><img className="sidebar_icons" src={sections} alt="Logo" />
           Section
         </NavLink>
         <NavLink
           to="/attendance"
-          className={location.pathname === "/attendance" ? "active" : ""}
+        className='sidebar-links'
+
           onClick={() => handleTabClick(4)}
-        >
+        ><img className="sidebar_icons" src={calender} alt="Logo" />
           Attendance
         </NavLink>
         <NavLink
           to="/reports"
-          className={location.pathname === "/reports" ? "active" : ""}
+        className='sidebar-links'
+
           onClick={() => handleTabClick(5)}
-        >
+        ><img className="sidebar_icons" src={report} alt="Logo" />
           Report
         </NavLink>
         
       </nav>
       <h3>
-        <NavLink to="#" onClick={props.logout} className="logout">
+        <NavLink to="#" onClick={props.logout} className="logout sidebar-links">
+        <img className="sidebar_icons" src={logout} alt="Logo" />
           Logout
         </NavLink>
       </h3>
