3edf40ba201e2ecea33aed1fb72114c22387c6f3
MouhamadAlAssaad <mhamad.said.2182000@gmail.com>
2023-03-07 11:53:35 +0200

    sidebar

--- lms/src/App.js
+++ lms/src/App.js
@@ -1,27 +1,28 @@
-import { Route,Routes } from 'react-router-dom';
-import './App.css';
-import Sidebar from './Dashboard/sidebar';
-import Admins from './pages/admins'
-import Students from './pages/students'
-import Classes from './pages/classes'
-import Sections from './pages/sections'
-import Attendance from './pages/attendance'
-import Reports from './pages/reports'
-
+import { Route, Routes} from "react-router-dom";
+import "./App.css";
+import Sidebar from "./Dashboard/sidebar";
+import Admins from "./pages/admins";
+import Students from "./pages/students";
+import Classes from "./pages/classes";
+import Sections from "./pages/sections";
+import Attendance from "./pages/attendance";
+import Reports from "./pages/reports";
+import Topbar from "./topbar/topbar";
 
 function App() {
   return (
     <div className="App">
-      <Sidebar/>
-          <Routes>
-            <Route path="/admins" element={<Admins/>}/>
-            <Route path="/students" element={<Students/>}/>
-            <Route path="/classes" element={<Classes/>}/>
-            <Route path="/sections" element={<Sections/>}/>
-            <Route path="/attendance" element={<Attendance/>}/>
-            <Route path="/reports" element={<Reports/>}/>
-          </Routes>
-   
+      <Topbar/>
+      <Sidebar />
+      <Routes>
+        <Route path="/admins" element={<Admins />} />
+        <Route path="/students" element={<Students />} />
+        <Route path="/classes" element={<Classes />} />
+        <Route path="/sections" element={<Sections />} />
+        <Route path="/attendance" element={<Attendance />} />
+        <Route path="/" element={<Reports />} />
+        
+      </Routes>
     </div>
   );
 }
--- lms/src/Dashboard/sidebar.js
+++ lms/src/Dashboard/sidebar.js
@@ -1,17 +1,14 @@
 import { useState } from "react";
-import {  useLocation , NavLink} from "react-router-dom";
-import "./sidebar.css"
-import logo from "./lms.svg"
-import calender from "./icons/calendar.png"
-import logout from "./icons/logout.png"
-import presentation from "./icons/presentation.png"
-import report from "./icons/report.png"
-import sections from "./icons/sections.png"
-import student from "./icons/student.png"
-import user from "./icons/user.png"
-
-
-
+import { useLocation, NavLink } from "react-router-dom";
+import "./sidebar.css";
+import logo from "./lms.svg";
+import calender from "./icons/calendar.png";
+import logout from "./icons/logout.png";
+import presentation from "./icons/presentation.png";
+import report from "./icons/report.png";
+import sections from "./icons/sections.png";
+import student from "./icons/student.png";
+import user from "./icons/user.png";
 
 function Sidebar(props) {
   const [activeTab, setActiveTab] = useState(1);
@@ -23,63 +20,61 @@ function Sidebar(props) {
 
   return (
     <div className="sidebar">
-    <img className="sidebar_image" src={logo} alt="Logo" />
+      <img className="sidebar_image" src={logo} alt="Logo" />
       <nav>
-        
-        <NavLink 
+        <NavLink
+          to="/"
+          className="sidebar-links"
+          onClick={() => handleTabClick(5)}
+        >
+          <img className="sidebar_icons" src={report} alt="Logo" />
+          Dashboard
+        </NavLink>
+        <NavLink
           to="/admins"
-        className='sidebar-links'
-
+          className="sidebar-links"
           onClick={() => handleTabClick(1)}
-
-        >    <img className="sidebar_icons" src={user} alt="Logo" />
-
+        >
+          {" "}
+          <img className="sidebar_icons" src={user} alt="Logo" />
           Admin
         </NavLink>
         <NavLink
           to="/students"
-          className='sidebar-links'
-
+          className="sidebar-links"
           onClick={() => handleTabClick(1)}
-        ><img className="sidebar_icons" src={student} alt="Logo" />
+        >
+          <img className="sidebar_icons" src={student} alt="Logo" />
           Students
         </NavLink>
         <NavLink
           to="/classes"
-          className='sidebar-links'
-
+          className="sidebar-links"
           onClick={() => handleTabClick(2)}
-        ><img className="sidebar_icons" src={presentation} alt="Logo" />
+        >
+          <img className="sidebar_icons" src={presentation} alt="Logo" />
           Class
         </NavLink>
         <NavLink
-        className='sidebar-links'
+          className="sidebar-links"
           to="/sections"
           onClick={() => handleTabClick(3)}
-        ><img className="sidebar_icons" src={sections} alt="Logo" />
+        >
+          <img className="sidebar_icons" src={sections} alt="Logo" />
           Section
         </NavLink>
         <NavLink
           to="/attendance"
-        className='sidebar-links'
-
+          className="sidebar-links"
           onClick={() => handleTabClick(4)}
-        ><img className="sidebar_icons" src={calender} alt="Logo" />
+        >
+          <img className="sidebar_icons" src={calender} alt="Logo" />
           Attendance
         </NavLink>
-        <NavLink
-          to="/reports"
-        className='sidebar-links'
-
-          onClick={() => handleTabClick(5)}
-        ><img className="sidebar_icons" src={report} alt="Logo" />
-          Report
-        </NavLink>
-        
       </nav>
       <h3>
-        <NavLink to="#" onClick={props.logout} className="logout sidebar-links">
-        <img className="sidebar_icons" src={logout} alt="Logo" />
+        <NavLink to="#" onClick={props.logout} className="logout ">
+          <img className="sidebar_icons" src={logout} alt="Logo" />
           Logout
         </NavLink>
       </h3>
